9911347b05333377496c09bd74e250b8ad70451c
icestone59 <icestone59@gmail.com>
2026-08-13 15:33:28 +0700

    Update finance.py

--- interpreters/finance.py
+++ interpreters/finance.py
@@ -3,12 +3,13 @@ from .base import BaseInterpreter
 class FinanceInterpreter(BaseInterpreter):
     def analyze(self, planets: dict, houses: dict) -> dict:
         category = "finance"
+        
         h2_sign = houses['House_2']['sign']
         h8_sign = houses['House_8']['sign']
-        venus_sign = planets['Venus']['sign']
+        venus_key = f"Venus_{planets['Venus']['sign']}_H{planets['Venus']['house']}"
 
         return {
-            "income_structure": self.fetch_text(category, f"H2_{h2_sign}"),
-            "shared_resources": self.fetch_text(category, f"H8_{h8_sign}"),
-            "attraction_style": self.fetch_text(category, f"Venus_{venus_sign}")
+            "self_earned_income_h2": self.get_content(category, f"H2_{h2_sign}"),
+            "shared_assets_investment_h8": self.get_content(category, f"H8_{h8_sign}"),
+            "wealth_attraction_venus": self.get_content(category, venus_key)
         }
